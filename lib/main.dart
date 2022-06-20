4cacd2708cea5cf474d945c23fcbdce070388660
ethanlee <seungwoonlee90@gmail.com>
2022-06-20 21:55:20 +0900

    Update AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,14 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+            color : Colors.white,
+            elevation: 1,
+            titleTextStyle: TextStyle(color: Colors.black, fontSize: 25),
+            actionsIconTheme: IconThemeData(color: Colors.black)
+        )
+      ),
       home : MyApp())
   );
 }
@@ -12,7 +20,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text("Wonderwall"),
+        centerTitle: false,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add_box_outlined),
+            onPressed: (){},
+            iconSize: 30,
+          )],),
       body: Text("You're my wonderwall")
     );
   }
