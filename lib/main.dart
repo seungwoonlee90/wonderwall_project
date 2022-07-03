ee8e71385f26c50cd3ea4dcbeea38737249a49d7
ethanlee <seungwoonlee90@gmail.com>
2022-07-04 00:10:26 +0900

    install provider

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:image_picker/image_picker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
 import 'dart:io';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MaterialApp(
